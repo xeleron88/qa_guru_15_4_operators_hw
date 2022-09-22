f350f188c0c5b52a6dcc25307dfe1d7756b4da84
Evgeniy <xelseo@gmail.com>
2022-09-22 20:48:41 +0300

    Doing homework

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 15;
+        double c = 1.5;
+
+        System.out.println("Result is " + (a + b) / c);
+        System.out.println("Result is " + (a + b) % c);
+
+        byte maxByte = (byte) (Byte.MAX_VALUE + 1);
+        System.out.println(maxByte + " = Max_byte + 1");
+        short minShort = (short) (Short.MIN_VALUE - 1) ;
+        System.out.println(minShort + " = Min_short - 1");
+
+        if ((a + b) % c == 0 || (a + b) / c != 0) {
+            System.out.println("Hello, World!");
+        }
+
+        for (int i = 1; i <= 25; i+=5) {
+            if (i % 2 == 0 ) {
+                System.out.println("Number " + i + " is even");
+            } else {
+                System.out.println("Number " + i + " is odd");
+            }
+        }
+        String ternary = a > 10 ? "Number is greater than 10"
+                : (a > 5 ? "Number is greater than 5" : "Number is less than equal to 5");
+        System.out.println(ternary);
+    }
+}
